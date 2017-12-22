497f8c07e622850b49f8f74625623b2be2b0d458
brokerstir <webdev@brokerstir.com>
2017-12-21 22:08:35 -0500

    initial commit

---
+++ login.php
@@ -0,0 +1,83 @@
+<?php // Do not put any HTML above this line
+session_start();
+
+if ( isset($_POST['cancel'] ) ) {
+    // Redirect the browser to game.php
+    header("Location: index.php");
+    return;
+}
+
+$salt = 'XyZzy12*_';
+$stored_hash = 'a8609e8d62c043243c4e201cbb342862';  // Pw is meow123
+
+// Check to see if we have some POST data, if we do process it
+if ( isset($_POST['who']) && isset($_POST['pass']) ) {
+
+    unset($_SESSION["who"]);  // Logout current user
+
+    if ( strlen($_POST['who']) < 1 || strlen($_POST['pass']) < 1 ) {
+
+        $_SESSION["error"] = "Email and password are required";
+        header( 'Location: login.php' ) ;
+        return;
+
+    } else if (!filter_var($_POST['who'], FILTER_VALIDATE_EMAIL)) {
+
+        $_SESSION["error"] = "Email must have an at-sign (@)";
+        header( 'Location: login.php' ) ;
+        return;
+
+    } else {
+
+        $check = hash('md5', $salt.$_POST['pass']);
+        if ( $check == $stored_hash ) {
+            // Redirect the browser to game.php
+            error_log("Login success ".$_POST['who']);
+            $_SESSION["who"] = $_POST["who"];
+            $_SESSION["success"] = "Logged in.";
+            header( 'Location: view.php' ) ;
+            return;
+
+        } else {
+            error_log("Login fail ".$_POST['who']);
+            $_SESSION["error"] = "Incorrect password";
+            header( 'Location: login.php' ) ;
+            return;
+        }
+    }
+} // end if ( isset($_POST['who']) && isset($_POST['pass']) )
+
+// Fall through into the View
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php require_once "bootstrap.php"; ?>
+<title>Robert Risk | Tracking Autos</title>
+</head>
+<body>
+<div class="container">
+<h1>Please Log In</h1>
+<?php
+
+if ( isset($_SESSION["error"]) ) {
+      echo('<p style="color:red">'.$_SESSION["error"]."</p>\n");
+      unset($_SESSION["error"]);
+  }
+?>
+<form method="POST">
+<label for="nam">User Name</label>
+<input type="text" name="who" id="nam"><br/>
+<label for="id_1723">Password</label>
+<input type="text" name="pass" id="id_1723"><br/>
+<input type="submit" value="Log In">
+<input type="submit" name="cancel" value="Cancel">
+</form>
+<p>
+For a password hint, view source and find a password hint
+in the HTML comments.
+<!-- Hint: The password is the four character sound a cat
+makes (all lower case) followed by 123. -->
+</p>
+</div>
+</body>
